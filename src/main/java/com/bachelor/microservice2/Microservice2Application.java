4a2388b489d7d83e9d6f8ac45b4b322a958fd0b3
marijatrajkova <marija.trajkova@endava.com>
2020-03-30 17:37:04 +0200

    Implement eureka discovery

---
+++ src/main/java/com/bachelor/microservice2/Microservice2Application.java
@@ -0,0 +1,24 @@
+package com.bachelor.microservice2;
+
+import client.ConsumerControllerClient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Microservice2Application {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(Microservice2Application.class, args);
+        ConsumerControllerClient consumerControllerClient = context.getBean(ConsumerControllerClient.class);
+        System.out.println(consumerControllerClient);
+        consumerControllerClient.getEmployee();
+    }
+
+    @Bean
+    public ConsumerControllerClient consumerControllerClient() {
+        return new ConsumerControllerClient();
+    }
+
+}
