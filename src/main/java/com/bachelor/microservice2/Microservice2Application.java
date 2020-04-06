8382953d42f159f3d2420831ed4000175e7e6080
marijatrajkova <marija.trajkova@endava.com>
2020-04-06 15:41:11 +0200

    remove unnecessary code

--- src/main/java/com/bachelor/microservice2/Microservice2Application.java
+++ src/main/java/com/bachelor/microservice2/Microservice2Application.java
@@ -1,6 +1,5 @@
 package com.bachelor.microservice2;
 
-import com.bachelor.microservice2.client.ConsumerControllerClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -12,9 +11,6 @@ public class Microservice2Application {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(Microservice2Application.class, args);
-        ConsumerControllerClient consumerControllerClient = context.getBean(ConsumerControllerClient.class);
-        System.out.println(consumerControllerClient);
-        consumerControllerClient.getEmployee();
     }
 
 }
